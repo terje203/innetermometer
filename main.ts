9149276ad7a3393f49223a55c58ee1298ac4deca
terje203 <126720443+terje203@users.noreply.github.com>
2023-03-05 11:59:50 +0100

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
+// Her defineres variabelen utetemp etter signalvedrdien som kommer over radio
 radio.onReceivedNumber(function (receivedNumber) {
     utetemp = receivedNumber
 })
+// Knapp a viser denne bittens temperatur
+// 
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(input.temperature())
 })
+// b viser utetemperatur fra radiosignalet til den andre bitten
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(utetemp)
 })
 let utetemp = 0
+// Vi setter radiogruppe som er lik ute micro:bitten
+// 
 radio.setGroup(23)
